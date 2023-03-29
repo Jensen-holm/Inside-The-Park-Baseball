eb6ef2d1c695cec757729e61483e53049374681f
Jensen Holm <holmj@mail.gvsu.edu>
2023-03-29 16:33:31 -0400

    cant figure out why the button is not showing up

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-pub mod gui;
-
 use gtk::prelude::*;
+use gtk::{glib, Align, Application, ApplicationWindow, Button};
 
-fn main() {
-    let app = gtk::Application::builder()
-        .application_id("com.Jensen.BaseballSim")
+fn main() -> glib::ExitCode {
+    let application = Application::new(
+        Some("com.github.gtk-rs.examples.builder_pattern"),
+        Default::default(),
+    );
+    application.connect_activate(build_ui);
+    application.run()
+}
+
+fn build_ui(application: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(application)
+        .title("First GTK Program")
+        .default_width(350)
+        .default_height(70)
         .build();
 
-    app.connect_activate(gui::on_activate);
-    app.run();
+    let button = Button::builder()
+        .margin_top(10)
+        .margin_bottom(10)
+        .margin_start(10)
+        .margin_end(10)
+        .halign(Align::Center)
+        .valign(Align::Center)
+        .label("Click Me!")
+        .build();
+
+    window.set_child(Some(&button));
+
+    window.show();
 }
