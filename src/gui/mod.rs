e5f63892ce08088e0ef50db650c9640045db06ca
Jensen Holm <holmj@mail.gvsu.edu>
2023-03-29 16:01:49 -0400

    changed code to follow official gtk documentation

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,35 +1,18 @@
+mod widgets;
+
 use gtk::prelude::*;
-use gtk::{Button, Window, WindowType, Entry, Grid, Label};
 
-
-pub(crate) fn main() {
-    if gtk::init().is_err() {
-        panic!("Failed to initialize GTK");
-    }
-
-    let window = Window::new(WindowType::Toplevel);
-    window.set_title("Baseball Sim Hub");
-    window.set_default_size(
-        1200,
-        800,
-    );
-
-    let team_grid = Grid::new();
-    team_grid.set_row_homogeneous(true);
-    team_grid.set_column_homogeneous(true);
-
-    // team entry label
-    let team_entry_label = Label::new(Some("Enter Teams: "));
-    team_grid.attach(&team_entry_label, 0, 0, 1, 1);
-
-    // team entry boxes
-    let team1 = Entry::new();
-    team1.set_size_request(1, 10);
-    team1.set_text("Team 1");
-    team_grid.attach(&team1, 1, 0, 1, 1);
-
-    window.add(&team_grid);
-    window.show_all();
-    gtk::main();
+fn on_activate(app: &gtk::Application) {
+    let win = gtk::ApplicationWindow::new(app);
+    win.set_size_request(1200, 800);
+    win.present();
 }
 
+pub(crate) fn main() {
+    let app = gtk::Application::builder()
+        .application_id("jensen")
+        .build();
+
+    app.connect_activate(on_activate);
+    app.run();
+}
---
+++ src/gui/widgets.rs
@@ -0,0 +1,6 @@
+use gtk::{Button, Grid, Entry};
+
+// this crate holds our custom widgets for
+// this specific application
+
+
