29b9c1f686049efd74fd9a7f33a707db92c4bf96
Jensen Holm <holmj@mail.gvsu.edu>
2023-03-29 15:37:29 -0400

    working on adding a simulation button

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,28 +1,35 @@
-extern crate gtk;
-
 use gtk::prelude::*;
-use gtk::{Button, Window, WindowType};
+use gtk::{Button, Window, WindowType, Entry, Grid, Label};
 
 
 pub(crate) fn main() {
     if gtk::init().is_err() {
-        println!("Failed to initialize GTK.");
-        return;
+        panic!("Failed to initialize GTK");
     }
 
     let window = Window::new(WindowType::Toplevel);
-    window.set_title("Hello, World!");
-    window.set_default_size(350, 70);
+    window.set_title("Baseball Sim Hub");
+    window.set_default_size(
+        1200,
+        800,
+    );
 
-    let button = Button::with_label("Click me!");
-    button.connect_clicked(|_| {
-        println!("Hello, World!");
-    });
+    let team_grid = Grid::new();
+    team_grid.set_row_homogeneous(true);
+    team_grid.set_column_homogeneous(true);
 
-    window.add(&button);
+    // team entry label
+    let team_entry_label = Label::new(Some("Enter Teams: "));
+    team_grid.attach(&team_entry_label, 0, 0, 1, 1);
 
+    // team entry boxes
+    let team1 = Entry::new();
+    team1.set_size_request(1, 10);
+    team1.set_text("Team 1");
+    team_grid.attach(&team1, 1, 0, 1, 1);
+
+    window.add(&team_grid);
     window.show_all();
-
     gtk::main();
 }
 
